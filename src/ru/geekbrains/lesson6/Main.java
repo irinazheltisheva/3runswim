1da5f997c29474b6b4206aa062a5ce0b3b5300b5
Irina <70594426+irinazheltisheva@users.noreply.github.com>
2021-06-02 10:09:17 +0300

    Initial commit

---
+++ src/ru/geekbrains/lesson6/Main.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.lesson6;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        class Animal {
+
+            public void voice() {
+            }
+        }
+        System.out.println("Run:");
+        System.out.println("cat 200 м");
+        System.out.println("dog 500 м");
+        System.out.println("Swim:");
+        System.out.println("cat no swim");
+        System.out.println("dog 10 м");
+
+
+
+    }
+}
+
